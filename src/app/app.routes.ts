77a77fdb56d0209c19b036107050290547e0a0a6
Caio <caio.almeida16.ca@gmail.com>
2026-09-02 19:12:14 -0300

    feat: Configuration commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    children: [
+      {
+        path: "",
+        loadChildren: () => import("./modules/modules.routes").then((r) => r.MODULES_ROUTES),
+      },
+      {
+        path: "",
+        redirectTo: "remote-entry",
+        pathMatch: "full",
+      },
+    ],
+  },
+];
